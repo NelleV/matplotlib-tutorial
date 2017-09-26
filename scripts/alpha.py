3b64f29e33da67d3da77c54904a55cca901b6414
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2017-09-26 08:30:40 -0700

    Small modifications to use the oo-interface of matplotlib

--- scripts/alpha.py
+++ scripts/alpha.py
@@ -1,15 +1,16 @@
 from pylab import *
+import matplotlib.pyplot as plt
 
 size = 256,16
 dpi = 72.0
 figsize= size[0]/float(dpi),size[1]/float(dpi)
-fig = figure(figsize=figsize, dpi=dpi)
+fig = plt.figure(figsize=figsize, dpi=dpi)
 fig.patch.set_alpha(0)
-axes([0,0.1,1,.8], frameon=False)
+ax = fig.add_axes([0,0.1,1,.8], frameon=False)
 
 for i in range(1,11):
-    plt.axvline(i, linewidth=1, color='blue',alpha=.25+.75*i/10.)
+    ax.axvline(i, linewidth=1, color='blue',alpha=.25+.75*i/10.)
 
-xlim(0,11)
-xticks([]),yticks([])
+ax.set_xlim(0,11)
+ax.set_xticks([]),yticks([])
 savefig('../figures/alpha.png', dpi=dpi)
